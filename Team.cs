38a3a87a2477604e215ecf06c9d371ca1d8d1a0b
Olga0710 <olga.berezhnaya20006@gmail.com>
2025-10-20 18:43:02 +0300

    Add files via upload

---
+++ Team.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerTeam
+{
+    public class Team
+    {
+        public string _teamName { get; set; } 
+        private List<Worker> workers;
+        public Team (string name)
+        {
+            _teamName = name;
+            workers = new List<Worker>();
+        }
+
+        public void Addworker(Worker worker)
+        {
+            workers.Add(worker);
+        }
+        
+        public void Print_info()
+        {
+            Console.WriteLine($"Name of team: {_teamName}");
+            Console.WriteLine("Workers: ");
+            foreach(var worker in workers)
+            {
+                Console.WriteLine(worker._name);
+            }
+        }
+
+        public void Print_detailed_info()
+        {
+            Console.WriteLine($"Name of team: {_teamName}");
+            foreach(var worker in workers)
+            {
+                Console.WriteLine($"{worker._name} - {worker._position} - {worker._workday}");
+            }
+
+        }
+    
+    }
+}
