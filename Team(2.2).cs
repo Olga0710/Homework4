04cc1ba2ecf0514982ad06d0bb922c5157654900
Olga0710 <olga.berezhnaya20006@gmail.com>
2025-10-27 00:25:23 +0200

    Update Team(2.2).cs

--- Team(2.2).cs
+++ Team(2.2).cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +8,9 @@ namespace WorkerTeam
 {
     public class Team
     {
-        public string _teamName { get; set; } 
+        public string _teamName { get; set; }
         private List<Worker> workers;
-        public Team (string name)
+        public Team(string name)
         {
             _teamName = name;
             workers = new List<Worker>();
@@ -20,26 +20,26 @@ namespace WorkerTeam
         {
             workers.Add(worker);
         }
-        
-        public void Print_info()
+
+        public void PrintInfo()
         {
             Console.WriteLine($"Name of team: {_teamName}");
             Console.WriteLine("Workers: ");
-            foreach(var worker in workers)
+            foreach (var worker in workers)
             {
                 Console.WriteLine(worker._name);
             }
         }
 
-        public void Print_detailed_info()
+        public void PrintDetailedInfo()
         {
             Console.WriteLine($"Name of team: {_teamName}");
-            foreach(var worker in workers)
+            foreach (var worker in workers)
             {
                 Console.WriteLine($"{worker._name} - {worker._position} - {worker._workday}");
             }
 
         }
-    
+
     }
 }
