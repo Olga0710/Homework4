567c1c430f2f32f69adede5c1fae94f63eb6d388
Olga0710 <olga.berezhnaya20006@gmail.com>
2025-10-27 02:26:00 +0200

    Update Hw(3)program.cs

--- Hw(3)program.cs
+++ Hw(3)program.cs
@@ -1,43 +1,17 @@
-﻿namespace CurrencyConvert
+namespace CurrencyConvert
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            decimal usdRate = 41.95m;
-            decimal eurRate = 49.1m;
+            Console.Write("Enter the exchange rate for 1 EUR to UAH: ");
+            decimal euroRate = decimal.Parse(Console.ReadLine());
 
-            Convert convert = new Convert(usdRate, eurRate);
+            Console.Write("Enter the exchange rate for 1 USD to UAH: ");
+            decimal dollarRate = decimal.Parse(Console.ReadLine());
 
-            Console.WriteLine("How many UAH do you want to convert?");
-            if (!decimal.TryParse(Console.ReadLine(), out decimal amountUAH))
-            {
-                Console.WriteLine("Incorrect format of number");
-                return;
-            }
-            decimal convertedUSD = convert.uahTousd(amountUAH);
-            decimal convertedEUR = convert.uahToeur(amountUAH);
-            Console.WriteLine($"{amountUAH:F2} UAH = {convertedUSD:F2} USD");
-            Console.WriteLine($"{amountUAH:F2} UAH = {convertedEUR:F2} EUR");
-
-            Console.WriteLine("How many USD do you want to convert?");
-            if (!decimal.TryParse(Console.ReadLine(), out decimal amountUSD))
-            {
-                Console.WriteLine("Incorrect format of number");
-                return;
-            }
-            decimal convertedUAH1 = convert.usdTouah(amountUSD);
-            Console.WriteLine($"{amountUSD:F2} USD = {convertedUAH1:F2} UAH");
-
-            Console.WriteLine("How many EUR do you want to convert?");
-            if (!decimal.TryParse(Console.ReadLine(), out decimal amountEUR))
-            {
-                Console.WriteLine("Incorrect format of number");
-                return;
-            }
-            decimal convertedUAH2 = convert.usdTouah(amountEUR);
-            Console.WriteLine($"{amountEUR:F2} EUR = {convertedUAH2:F2} UAH");
-
+            Converter converter = new Converter(euroRate, dollarRate);
+            converter.Convert();
         }
     }
 }
